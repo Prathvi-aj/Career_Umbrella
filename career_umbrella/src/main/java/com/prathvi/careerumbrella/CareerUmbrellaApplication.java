1300be779c9050c6cf4ea6ad746c15b503d6e549
Abhishek Jain <56496325+Prathvi-aj@users.noreply.github.com>
2024-04-13 13:56:26 +0530

    backend service

---
+++ career_umbrella/src/main/java/com/prathvi/careerumbrella/CareerUmbrellaApplication.java
@@ -0,0 +1,13 @@
+package com.prathvi.careerumbrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerUmbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerUmbrellaApplication.class, args);
+	}
+
+}
